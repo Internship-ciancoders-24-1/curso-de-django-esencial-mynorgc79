c95c0d6062255d3cafa4d398169aad5c8af44bc2
mynorgc79 <mynorguarchaj99@gmail.com>
2024-03-06 15:36:49 -0600

    feat: Dia 3. Capitulo 4, curso Django

--- platzigram/platzigram/urls.py
+++ platzigram/platzigram/urls.py
@@ -8,6 +8,7 @@ from django.conf import settings
 from platzigram import views as local_views
 from posts import views as posts_views
 from . import views
+from users import views as users_views
 
 
 
@@ -19,5 +20,7 @@ urlpatterns = [
     path("hi/<str:name>/<int:age>/",views.say_hi),
 
     path('posts/', posts_views.list_posts),
+
+    path('user/login', users_views.login_view, name='login'),
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- platzigram/users/views.py
+++ platzigram/users/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
+
+
+def login_view(request):
+    """Login view."""
+    return render(request, "login.html")
